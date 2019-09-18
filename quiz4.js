f0794c9e4cc04bb7901ea9068934866831027532
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-09-18 14:07:02 -0400

    Update quiz4.js

--- quiz4.js
+++ quiz4.js
@@ -3,7 +3,7 @@ const dejon = {
     nameLast : `Kurti`,
     mass : 110,
     height : 1.95,
-    getBMI : function() {
+    bmi : function() {
     return this.mass / (this.height * this.height);
     },
     // bmi = this.getBMI()
@@ -16,7 +16,7 @@ const rob = {
     nameLast : `Grande`,
     mass : 78,
     height : 1.69,
-    getBMI : function() {
+    bmi : function() {
     return this.mass / (this.height * this.height);
     },
     // bmi = this.getBMI()
@@ -24,10 +24,10 @@ const rob = {
 
 rob.bmi();
 
-if (dejon.bmi > rob.bmi){
+if (dejon.bmi() > rob.bmi()){
     console.log(`Dejon's BMI is greater than Rob's.`);
-} else if (dejon.bmi < rob.bmi){
+} else if (dejon.bmi() < rob.bmi()){
     console.log(`Rob's BMI is greater than Dejon's.`);
-} else if (dejon.bmi == rob.bmi){
+} else if (dejon.bmi() == rob.bmi()){
     console.log(`Their BMIs are equal.`);
 }
